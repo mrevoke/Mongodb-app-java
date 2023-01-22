533d44f529770aa21edac1e3b58086a02b3029db
Jeet Singh <jeet29113@gmail.com>
2023-01-22 22:29:00 +0530

    MONGO DB JAVA APP

--- app/src/main/java/com/example/mongo/MainActivity.java
+++ app/src/main/java/com/example/mongo/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.mongo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import io.realm.Realm;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -15,6 +20,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         Realm.init(this);
 
+       Button B=findViewById(R.id.button);
+       B.setOnClickListener(new View.OnClickListener() {
+            @Override
 
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this,CRUD.class);
+                startActivity(intent);
+            } });
     }
+
+
+
 }
--- app/src/main/java/com/example/mongo/SignUpActivity.java
+++ app/src/main/java/com/example/mongo/SignUpActivity.java
@@ -1,8 +1,5 @@
 package com.example.mongo;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,7 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import io.realm.Realm;
 import io.realm.mongodb.App;
@@ -25,6 +23,8 @@ public class SignUpActivity extends AppCompatActivity {
     String Apid = "mongo-dvatp";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         Realm.init(this);
         App app = new App(new AppConfiguration.Builder(Apid).build());
 
